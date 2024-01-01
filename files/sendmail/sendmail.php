236783d5071b76b770d399961b95d6120c6cc52c
gajovich <75524971+gajovich@users.noreply.github.com>
2024-01-01 15:02:19 +0200

    Add files via upload

--- files/sendmail/sendmail.php
+++ files/sendmail/sendmail.php
@@ -5,37 +5,39 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+// require 'phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->setLanguage('ru', 'phpmailer/language/');
 $mail->IsHTML(true);
 
-/*
-	$mail->isSMTP();                                            //Send using SMTP
-	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	$mail->Username   = 'user@example.com';                     //SMTP username
-	$mail->Password   = 'secret';                               //SMTP password
-	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-	$mail->Port       = 465;                 
-	*/
 
-//Від кого лист
-$mail->setFrom('gajovich79@gmail.com', 'Фрілансер по життю'); // Вказати потрібний E-mail
-//Кому відправити
+//From whom the letter
+$mail->setFrom('gajovich79@gmail.com', 'Mailer');
+
 $mail->addAddress('gajovich79@gmail.com'); // Вказати потрібний E-mail
-//Тема листа
-$mail->Subject = 'Вітання! Це "Фрілансер по життю"';
+//Leaf theme
+$mail->Subject = 'Hello! This is "Surething Remodeling"';
 
-//Тіло листа
-$body = '<h1>Зустрічайте супер листа!</h1>';
+//The body of the letter
+$body = "<h1>Meet Super Letter!</h1>";
 
-//if(trim(!empty($_POST['email']))){
-//$body.=$_POST['email'];
-//}	
-
+if (trim(!empty($_POST['name']))) {
+	$body .= '<p><strong>Name:</strong> ' . $_POST['name'] . '</p>';
+}
+if (trim(!empty($_POST['email']))) {
+	$body .= '<p><strong>Email:</strong> ' . $_POST['email'] . '</p>';
+}
+if (trim(!empty($_POST['phone']))) {
+	$body .= '<p><strong>Phone:</strong> ' . $_POST['phone'] . '</p>';
+}
+if (trim(!empty($_POST['address']))) {
+	$body .= '<p><strong>Address:</strong> ' . $_POST['address'] . '</p>';
+}
+if (trim(!empty($_POST['message']))) {
+	$body .= '<p><strong>Message:</strong> ' . $_POST['message'] . '</p>';
+}
 
 //Прикріпити файл
 if (!empty($_FILES['image']['tmp_name'])) {
